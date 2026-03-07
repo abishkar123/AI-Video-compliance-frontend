a12bb9b7de21e7d6e9d115083a53939d211b0261
Abishkar Rai <raiabishkar0.5@gmail.com>
2026-03-07 14:24:45 +1100

    ready to deploy

--- src/pages/AuditPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import AuditForm   from '../components/audit/AuditForm';
-import AuditStatus from '../components/audit/AuditStatus';
-
-export default function AuditPage() {
-  const { sessionId } = useParams();
-  return sessionId ? <AuditStatus /> : <AuditForm />;
-}
